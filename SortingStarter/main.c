c89cf0b5860e56bb8de10aaa5e445330203ac238
Linhao Yuan <you@example.com>
2021-04-05 03:29:08 +0000

    tar xzf SortingStarter.tgz

---
+++ SortingStarter/main.c
@@ -0,0 +1,28 @@
+#include "file_lib.h"
+#include <stdio.h>
+	
+/*
+ * Read the file from the command-line.
+ * Usage:
+ * ./a.out FILE_TO_READ FILE_TO_WRITE
+ *
+ */
+
+int main(int argc, char** argv){
+	// Read the original file.
+	
+
+	// Sort the file with the function you wrote.
+	
+
+	// Write out the new file.
+	
+
+}
+
+// You can see if your file worked correctly by using the
+// command:
+//
+// diff ORIGINAL_FILE NEW_FILE
+//
+// If the command returns ANYTHING the files are different.
